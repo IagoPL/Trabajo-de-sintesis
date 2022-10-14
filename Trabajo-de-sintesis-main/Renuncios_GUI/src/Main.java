fed70b8017a6631337b6a446d37a1c1aaf45b853
Iago Prieto Lamas <50492345+IagoPL@users.noreply.github.com>
2022-10-14 19:11:11 +0200

    funciona cambio de ventanas

---
+++ Trabajo-de-sintesis-main/Renuncios_GUI/src/Main.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.io.IOException;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException, FontFormatException {
+
+
+        exect();
+
+
+    }
+
+
+    public static void exect() throws IOException, FontFormatException {
+
+
+                    LoginPanel Login = new LoginPanel();
+
+
+    }
+
+}
---
+++ Trabajo-de-sintesis-main/Renuncios_GUI/src/Utilities.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.*;
+
+public class Utilities {
+
+    //Constructor Vacío
+
+    public Utilities() {
+
+    }
+
+    boolean loginOpened = true;
+    boolean MainAppToBeOpened = false;
+
+
+    public String primaryColor() {
+        return "#beafea"; //retornar fuente
+    }
+
+    public void font() {
+        return ;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
